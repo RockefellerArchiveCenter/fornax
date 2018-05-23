5879151e394d8c32e84fe1ffa174700ffd6b9381
Hillel Arnold <helrond@hotmail.com>
2018-05-23 14:21:52 -0400

    adding data via accessions

--- sip_assembly/serializers.py
+++ sip_assembly/serializers.py
@@ -1,17 +1,9 @@
 from rest_framework import serializers
-from sip_assembly.models import SIP, RightsStatement
-
-
-class RightsStatementSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = RightsStatement
-        exclude = ('id', 'sip')
+from sip_assembly.models import SIP
 
 
 class SIPSerializer(serializers.HyperlinkedModelSerializer):
-    rights_statements = RightsStatementSerializer(many=True)
 
     class Meta:
         model = SIP
-        fields = '__all__'
+        fields = ('url', 'bag_identifier', 'aurora_uri', 'data', 'created', 'last_modified')
