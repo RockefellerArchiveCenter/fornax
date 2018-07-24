4f4b80703ee5fa776e760ddfabd3d4ef56354d69
Hillel Arnold <helrond@hotmail.com>
2018-07-24 13:50:05 -0400

    updating for combined docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # Apply database migrations
+./wait-for-it.sh db:5432 -- echo "Creating config file"
+
+if [ ! -f manage.py ]; then
+  cd fornax
+fi
+
 echo "Apply database migrations"
-./wait-for-it.sh db:5432 -- python manage.py migrate
+if [ ! -f fornax/config.py ]; then
+    cp fornax/config.py.example fornax/config.py
+fi
+
+python manage.py migrate
 
 # Create users
 echo "Create users"
 python manage.py shell -c "from django.contrib.auth.models import User; \
   User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
 
-if [ ! -f fornax/config.py ]; then
-    echo "Creating config file"
-    cp fornax/config.py.example fornax/config.py
-fi
-
 #Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8002
--- fornax/settings.py
+++ fornax/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'fornax_db',
         'PORT': 5432,
     }
 }
