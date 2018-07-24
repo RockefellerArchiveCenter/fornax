365cfb5e4b0dc91174cf7c64e3127dd0dca80eaf
Hillel Arnold <helrond@hotmail.com>
2018-07-24 16:51:41 -0400

    updating ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,4 @@ python manage.py shell -c "from django.contrib.auth.models import User; \
 
 #Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8002
+python manage.py runserver 0.0.0.0:8003
