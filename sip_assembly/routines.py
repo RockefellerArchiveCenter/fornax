94518012c67d8592ecbe7e63ff0b05a7ce4337a6
Hillel Arnold <helrond@hotmail.com>
2020-02-28 17:01:47 -0500

    fix variable assignment

--- sip_assembly/routines.py
+++ sip_assembly/routines.py
@@ -159,7 +159,7 @@ class SIPActions(ArchivematicaRoutine):
                 else:
                     all_completed += completed.get('close_succeeded', [])
         return "All completed {} removed from dashboards {}".format(
-            type, ", ".join(dashboards)), completed
+            type, ", ".join(dashboards)), all_completed
 
 
 class CleanupRequester:
