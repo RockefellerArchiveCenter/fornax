1131c0a020751125eb9712ed72957328f40b69a9
bonniegee <bgordon@rockarch.org>
2022-01-25 16:11:10 -0500

    check process status

--- sip_assembly/tests.py
+++ sip_assembly/tests.py
@@ -237,10 +237,12 @@ class RoutineTests(TestCase):
         last_started.process_status = SIP.APPROVED
         last_started.archivematica_uuid = "12345"
         last_started.save()
+        sip = SIP.objects.filter(process_status=SIP.ASSEMBLED).first()
         mock_status.return_value = {'type': 'transfer', 'path': '/var/archivematica/sharedDirectory/currentlyProcessing/59193ace-30c3-4a3b-a656-9232ebc7ce0e.tar.gz', 'directory': '59193ace-30c3-4a3b-a656-9232ebc7ce0e.tar.gz', 'name': '59193ace-30c3-4a3b-a656-9232ebc7ce0e.tar.gz', 'uuid': '1651add2-d21b-445a-abd4-444450648ba9', 'microservice': 'Extract zipped bag transfer', 'status': 'PROCESSING', 'message': 'Fetched status for 1651add2-d21b-445a-abd4-444450648ba9 successfully.'}
         message, sip_id = StartPackageRoutine().run()
         self.assertEqual(message, "Another transfer is processing, waiting until it finishes.")
         self.assertEqual(len(sip_id), 1)
+        self.assertEqual(sip.process_status, SIP.ASSEMBLED)
 
     @patch("sip_assembly.routines.AMClient.close_completed_transfers")
     @patch("sip_assembly.routines.AMClient.close_completed_ingests")
